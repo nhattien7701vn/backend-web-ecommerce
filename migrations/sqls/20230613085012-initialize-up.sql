629eed9ce70b322854b59d87d04a0f26e4642217
nhattien7701vn <nhattien7701vn@gmail.com>
2023-06-13 17:34:09 +0700

    add fields

--- migrations/sqls/20230613085012-initialize-up.sql
+++ migrations/sqls/20230613085012-initialize-up.sql
@@ -1,16 +1,21 @@
 CREATE TABLE "product" (
   "id" integer PRIMARY KEY,
   "name" varchar,
-  "desc" varchar,
+  "uri" varchar,
+  "description" varchar,
   "category_id" integer,
   "price" decimal,
-  "created_at" timestamp
+  "created_at" timestamp,
+  "modified_at" timestamp
 );
 
 CREATE TABLE "category" (
   "id" integer PRIMARY KEY,
   "name" varchar,
-  "created_at" timestamp
+  "uri" varchar,
+  "description" varchar,
+  "created_at" timestamp,
+  "modified_at" timestamp
 );
 
 ALTER TABLE "product" ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");
