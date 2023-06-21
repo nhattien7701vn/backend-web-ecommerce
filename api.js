e93ab3b562c4cc0f26707e634218a67e7a41e446
nhattien7701vn <nhattien7701vn@gmail.com>
2023-06-21 10:56:54 +0700

    WIP deploy

--- api.js
+++ api.js
@@ -1,164 +1,169 @@
-const client = require('./connection.js')
+// const client = require('./connection.js')
+// const bodyParser = require('body-parser');
 const express = require('express');
+const app = express();
 const cors = require('cors');
 
-const app = express();
 app.use(cors());
+app.use(express.json());
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
 
-app.listen(3300, () => {
-  console.log("Sever is now listening at port 3300");
-})
-client.connect();
+// //HOME
+// app.get('/', function (req, res) {
+//   res.send("Hello world !!!")
+// })
 
-//HOME
-app.get('/', function (req, res) {
-  res.send("Hello world !!!")
-})
+// //GET ALL CATEGORY
 
-//GET ALL CATEGORY
-app.get('/categories', function (req, res) {
-  client.query(`select * from category`, (err, result) => {
-    if (!err) {
-      res.send(result.rows)
-    } else if (err) {
-      res.send(err.message)
-    };
-  })
-  console.log("GET request for the homepage");
-})
+// app.get('/categories', function (req, res) {
+//   client.query(`select * from category`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows)
+//     } else if (err) {
+//       res.send(err.message)
+//     };
+//   })
+//   console.log("GET request for the homepage");
+// })
 
-//GET CATEGORY BY ID
-app.get('/categories/:id', function (req, res) {
-  client.query(`Select * from category where id = '${req.params.id}'`, (err, result) => {
-    if (!err) {
-      res.send(result.rows)
-    } else if (err) {
-      res.send(err.message)
-    };
-  })
-  console.log("GET request for the homepage");
-})
+// //GET CATEGORY BY ID
 
-//CREATE NEW CATEGORY
-app.post('/categories', (req, res) => {
-  const category = req.body;
-  let insertQuery = `insert into category (id, name, uri, description, created_at, modified_at)
-                     values
-                     ('${category.id}', '${category.name}', '${category.uri}', '${category.description}', ${category.created_at}, ${category.modified_at});`
-  client.query(insertQuery, (err, result) => {
-    if (!err) {
-      res.send('Insertion was successful')
-    }
-    else { console.log(err.message) }
-  })
-  client.end;
-})
+// app.get('/categories/:id', function (req, res) {
+//   client.query(`Select * from category where id = '${req.params.id}'`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows)
+//     } else if (err) {
+//       res.send(err.message)
+//     };
+//   })
+//   console.log("GET request for the homepage");
+// })
 
-//UPDATE CATEGORY BY ID
-app.put('/categories/:id', (req, res) => {
-  let category = req.body;
-  let updateQuery = `update category
-                     set name = '${category.name}',
-                     uri = '${category.uri}',
-                     description = '${category.description}',
-                     modified_at = ${category.modified_at}
-                     where id = '${category.id}'`
+// //CREATE NEW CATEGORY
 
-  client.query(updateQuery, (err, result) => {
-    if (!err) {
-      res.send('Update was successful')
-    }
-    else { console.log(err.message) }
-  })
-  client.end;
-})
+// app.post('/categories', (req, res) => {
+//   const category = req.body;
+//   let insertQuery = `insert into category (id, name, uri, description, created_at, modified_at)
+//                      values
+//                      ('${category.id}', '${category.name}', '${category.uri}', '${category.description}', ${category.created_at}, ${category.modified_at});`
+//   client.query(insertQuery, (err, result) => {
+//     if (!err) {
+//       res.send('Insertion was successful')
+//     }
+//     else { console.log(err.message) }
+//   })
+//   client.end;
+// })
 
-//DELETE CATEGORY BY ID
-app.delete('/categories/:id', (req, res) => {
-  let insertQuery = `delete from category where id = '${req.params.id}'`
-  client.query(insertQuery, (err, result) => {
-    if (!err) {
-      res.send('Deletion was successful')
-    }
-    else { console.log(err.message) }
-  })
-  client.end;
-})
+// //UPDATE CATEGORY BY ID
 
-//PRODUCT 
+// app.put('/categories/:id', (req, res) => {
+//   let category = req.body;
+//   let updateQuery = `update category
+//                      set name = '${category.name}',
+//                      uri = '${category.uri}',
+//                      description = '${category.description}',
+//                      modified_at = ${category.modified_at}
+//                      where id = '${category.id}'`
 
-//GET ALL PRODUCT
-app.get('/products', function (req, res) {
-  client.query(`select * from product`, (err, result) => {
-    if (!err) {
-      res.send(result.rows)
-    } else if (err) {
-      res.send(err.message)
-    };
-  })
-  console.log("GET request for the homepage");
-})
+//   client.query(updateQuery, (err, result) => {
+//     if (!err) {
+//       res.send('Update was successful')
+//     }
+//     else { console.log(err.message) }
+//   })
+//   client.end;
+// })
 
-//GET PRODUCT BY ID
-app.get('/products/:id', function (req, res) {
-  client.query(`Select * from product where id = '${req.params.id}'`, (err, result) => {
-    if (!err) {
-      res.send(result.rows)
-    } else if (err) {
-      res.send(err.message)
-    };
-  })
-  console.log("GET request for the homepage");
-})
+// //DELETE CATEGORY BY ID
 
-//CREATE NEW PRODUCT
-app.post('/products', (req, res) => {
-  const product = req.body;
-  let insertQuery = `insert into product (id, name, uri, description, image_source, category_id, price, created_at, modified_at)
-                     values
-                     ('${product.id}', '${product.name}', '${product.uri}', '${product.description}', '${product.image_source}', '${product.category_id}', '${product.price}', ${product.created_at}, ${product.modified_at});`
-  client.query(insertQuery, (err, result) => {
-    if (!err) {
-      res.send('Insertion was successful')
-    }
-    else { console.log(err.message) }
-  })
-  client.end;
-})
+// app.delete('/categories/:id', (req, res) => {
+//   let insertQuery = `delete from category where id = '${req.params.id}'`
+//   client.query(insertQuery, (err, result) => {
+//     if (!err) {
+//       res.send('Deletion was successful')
+//     }
+//     else { console.log(err.message) }
+//   })
+//   client.end;
+// })
 
-//UPDATE PRODUCT BY ID //
-app.put('/products/:id', (req, res) => {
-  let product = req.body;
-  let updateQuery = `update product
-                     set name = '${product.name}',
-                     category_id = '${product.category_id}',
-                     uri = '${product.uri}',
-                     description = '${product.description}',
-                     image_source = '${product.image_source}',
-                     price = '${product.price}',
-                     modified_at = ${product.modified_at}
-                     where id = '${product.id}'`
+// //PRODUCT 
 
-  client.query(updateQuery, (err, result) => {
-    if (!err) {
-      res.send('Update was successful')
-    }
-    else { console.log(err.message) }
-  })
-  client.end;
-})
+// //GET ALL PRODUCT
 
-//DELETE PRODUCT BY ID
-app.delete('/products/:id', (req, res) => {
-  let insertQuery = `delete from product where id = '${req.params.id}'`
-  client.query(insertQuery, (err, result) => {
-    if (!err) {
-      res.send('Deletion was successful')
-    }
-    else { console.log(err.message) }
-  })
-  client.end;
-})
+// app.get('/products', function (req, res) {
+//   client.query(`select * from product`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows)
+//     } else if (err) {
+//       res.send(err.message)
+//     };
+//   })
+//   console.log("GET request for the homepage");
+// })
+
+// //GET PRODUCT BY ID
+
+// app.get('/products/:id', function (req, res) {
+//   client.query(`Select * from product where id = '${req.params.id}'`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows)
+//     } else if (err) {
+//       res.send(err.message)
+//     };
+//   })
+//   console.log("GET request for the homepage");
+// })
+
+// //CREATE NEW PRODUCT
+// app.post('/products', (req, res) => {
+//   const product = req.body;
+//   let insertQuery = `insert into product (id, name, uri, description, image_source, category_id, price, created_at, modified_at)
+//                      values
+//                      ('${product.id}', '${product.name}', '${product.uri}', '${product.description}', '${product.image_source}', '${product.category_id}', '${product.price}', ${product.created_at}, ${product.modified_at});`
+//   client.query(insertQuery, (err, result) => {
+//     if (!err) {
+//       res.send('Insertion was successful')
+//     }
+//     else { console.log(err.message) }
+//   })
+//   client.end;
+// })
+
+// //UPDATE PRODUCT BY ID //
+
+// app.put('/products/:id', (req, res) => {
+//   let product = req.body;
+//   let updateQuery = `update product
+//                      set name = '${product.name}',
+//                      category_id = '${product.category_id}',
+//                      uri = '${product.uri}',
+//                      description = '${product.description}',
+//                      image_source = '${product.image_source}',
+//                      price = '${product.price}',
+//                      modified_at = ${product.modified_at}
+//                      where id = '${product.id}'`
+
+//   client.query(updateQuery, (err, result) => {
+//     if (!err) {
+//       res.send('Update was successful')
+//     }
+//     else { console.log(err.message) }
+//   })
+//   client.end;
+// })
+
+// //DELETE PRODUCT BY ID
+
+// app.delete('/products/:id', (req, res) => {
+//   let insertQuery = `delete from product where id = '${req.params.id}'`
+//   client.query(insertQuery, (err, result) => {
+//     if (!err) {
+//       res.send('Deletion was successful')
+//     }
+//     else { console.log(err.message) }
+//   })
+//   client.end;
+// })
--- connection.js
+++ connection.js
@@ -1,10 +1,14 @@
 const { Client } = require('pg')
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '1234',
-  database: 'test',
+  // host: 'localhost',
+  // port: 5432,
+  // user: 'postgres',
+  // password: '1234',
+  // database: 'test',
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
-
+client.connect((err) => {
+  if (err) throw err
+  console.log("Connect to PostgreSQL successfully!")
+})
 module.exports = client;
